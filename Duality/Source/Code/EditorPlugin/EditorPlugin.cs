061910db0bd426018c17d6a4a069a2d9450ba6e2
krzysztof.kolek.praca@gmail.com <krzysztof.kolek.praca@gmail.com>
2016-08-30 09:59:42 +0200

    Init

---
+++ Duality/Source/Code/EditorPlugin/EditorPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality.Editor;
+
+namespace DualityCookieClickerClone.Editor
+{
+	/// <summary>
+	/// Defines a Duality editor plugin.
+	/// </summary>
+    public class DualityCookieClickerCloneEditorPlugin : EditorPlugin
+	{
+		public override string Id
+		{
+			get { return "DualityCookieClickerCloneEditorPlugin"; }
+		}
+	}
+}
